4d2c1b7a8c457ed0c9ae67eec74c59b95f55e65a
Massimo Santini <massimo.santini@gmail.com>
2020-05-05 17:24:59 +0200

    Deep refactoring of image names and paths

--- modules/cli/start-editor.py
+++ modules/cli/start-editor.py
@@ -1,26 +1,31 @@
 from csv import reader
+from itertools import cycle
+from os import environ
 from pathlib import Path
 from secrets import token_urlsafe
-from itertools import cycle
 
 import docker
+
+DANAKE_VERSION = environ['DANAKE_VERSION']
+EDITOR_IMAGE = '127.0.0.1:5000/danake/editor:' + DANAKE_VERSION
 
 UID2INFO_PATH = '/confs/uid2info.tsv'
 UID2INFO = dict(reader(Path(UID2INFO_PATH).open('r'), delimiter = '\t'))
 UIDS = sorted(UID2INFO.keys())
 
 connection = docker.from_env()
+
 HOSTS = sorted(node.attrs['Description']['Hostname'] for node in connection.nodes.list())
 
-print('deploycs: found hosts:', HOSTS)
+print('starteditor: found hosts:', HOSTS)
 
 for uid, host in list(zip(UIDS, cycle(HOSTS))):
-    print('deploycs: creating service: code-server-{}@{}'.format(uid, host))
+    print('starteditor: creating service: editor-{}@{}'.format(uid, host))
     connection.services.create(
-        image = '127.0.0.1:5000/danake/code-server:latest',
-        name = 'code-server-{}'.format(uid),
+        image = EDITOR_IMAGE,
+        name = 'editor-{}'.format(uid),
         constraints = ['node.hostname=={}'.format(host)],
-        mounts = ['coder-{}-volume:/home/coder/project'.format(uid)],
+        mounts = ['editor-{}-volume:/home/coder/project'.format(uid)],
         networks = ['danake_backend_network'],
-        labels = {'danake': 'code-server'}
+        labels = {'danake': 'editor'}
     )
