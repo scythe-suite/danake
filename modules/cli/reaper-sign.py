eea4806743d286e23f531a86d08ea05916bb96ae
Massimo Santini <santini@di.unimi.it>
2021-11-19 10:35:14 +0100

    Added tester to the signing process

--- modules/cli/reaper-sign.py
+++ modules/cli/reaper-sign.py
@@ -5,7 +5,8 @@ from sys import stdin
 
 UID2INFO = dict(reader(stdin, delimiter = '\t'))
 UIDS = sorted(UID2INFO.keys())
+REAPER_TESTER = environ.get('REAPER_TESTER', '')
 REAPER_SESSION = environ.get('REAPER_SESSION', '')
 
 for uid in UIDS:
-    check_call(['ssh', 'editor-{}'.format(uid), 'curl -sL http://reaper.srv.di.unimi.it/tm/santini/{}/{} | python3 | bash'.format(REAPER_SESSION, uid)])
+    check_call(['ssh', 'editor-{}'.format(uid), 'curl -sL http://reaper.srv.di.unimi.it/tm/{}/{}/{} | python3 | bash'.format(REAPER_TESTER, REAPER_SESSION, uid)])
